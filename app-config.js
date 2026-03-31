50c5f7c37beed01b65662dd11a7d638d12ee071a
Taha Tauquir <Taha24888@gmail.com>
2026-04-01 00:32:01 +0100

    Add app-config.js with API configuration

---
+++ app-config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  API_BASE:  "https://api.dev.sandbox.kewordal.com/cb"
+  ,
+  AUTH_TOKEN_URL:"https://auth.dev.sandbox.kewordal.com/realms/dev/protocol/openid-connect/token"
+};
