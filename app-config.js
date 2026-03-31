d9f7fd7a6605b3dfe2a331f502c33ac7b2345f02
Taha Tauquir <Taha24888@gmail.com>
2026-04-01 00:45:10 +0100

    Fix formatting in app-config.js

--- app-config.js
+++ app-config.js
@@ -1,5 +1,4 @@
 window.APP_CONFIG = {
-  API_BASE:  "https://api.dev.sandbox.kewordal.com/cb"
-  ,
+  API_BASE:  "https://api.dev.sandbox.kewordal.com/cb",
   AUTH_TOKEN_URL:"https://auth.dev.sandbox.kewordal.com/realms/dev/protocol/openid-connect/token"
 };
